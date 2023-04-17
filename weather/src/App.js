cdc6ed5127a0e79628d2df2c796a92c1a72836ee
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-04-17 17:30:52 -0400

    added pre style, and fetch from open api

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,8 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
+import { getWeather } from "./api/getWeather";
+import './App.css';
+
 
 const App = () => {
+    const [query, setQuery] = useState('')
+    const [weather, setWeather] = useState({})
+
+    const search = async (e) => {
+        if(e.key == 'Enter') {
+            const data = await getWeather(query)
+            console.log(data)
+
+            setWeather(data)
+            setQuery('')
+        }
+    }
+
+
     return (
-        <h1>App</h1>
+        <div className="main-container">
+            <input
+                type="text"
+                className="search"
+                placeholder="Search ..."
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyPress={search}
+
+            />
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name} </span>
+                        <sup>
+                            {weather.sys.country}
+                        </sup>
+                    </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup>&deg; C</sup>
+                    </div>
+                </div>
+
+            )}
+
+        </div>
     );
 }
 
